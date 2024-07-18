4506939f0d5df977f6b5865f890e1121cd0ad6eb
unknown <wasimmadha@gmail.com>
2024-07-19 01:27:56 +0530

    basic structure with dataset class

---
+++ dataset.py
@@ -0,0 +1,31 @@
+from torch.utils.data import Dataset
+import librosa
+import torch
+import numpy as np
+
+class SpecVocoderDataset(Dataset):
+    def __init__(self, training_files) -> None:
+        super().__init__()
+        self.training_files = training_files
+        self.sample_rate = 22050 # static value
+        self.n_fft = 1024 # static value
+        self.hop_length = 256 # static value
+        self.n_mels = 80 # static value
+    
+    def __len__(self):
+        return len(self.training_files)
+    
+    def __getitem__(self, idx):
+        waveform, sr = librosa.load(self.training_files[idx], sr=self.sample_rate)
+        waveform = waveform/32768.0 # static value
+        mel_spec = librosa.feature.melspectrogram(
+            y=waveform, 
+            sr=self.sample_rate, 
+            n_fft=self.n_fft, 
+            hop_length=self.hop_length, 
+            n_mels=self.n_mels
+        )
+        mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
+        mel_spec_db_tensor = torch.tensor(mel_spec_db)
+        waveform_tensor = torch.tensor(waveform)
+        return mel_spec_db_tensor, waveform_tensor
